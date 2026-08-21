9449e8c6e282726c58fcb2d1ffec72b803095f44
yanitxan <yanitxann@gmail.com>
2026-08-21 14:00:13 -0600

    changed dark mode to button to try to fix dark mode

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,17 +1,17 @@
 const toggleBtn = document.getElementById('theme-toggle');
 
-// Check for saved theme preference on page load
+
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
     document.body.classList.add('dark-mode');
 }
 
-// Listen for clicks on the toggle button
+
 toggleBtn.addEventListener('click', () => {
-    // Toggle the dark-mode class on the body
+
     document.body.classList.toggle('dark-mode');
     
-    // Save the preference to local storage
+
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('theme', 'dark');
     } else {
